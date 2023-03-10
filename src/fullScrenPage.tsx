e6baaf2d940af1fe6803c568b95ab3e2df7984ba
unel <86972822+UnelDev@users.noreply.github.com>
2023-03-10 21:46:46 +0100

    list product is functional

--- src/fullScrenPage.tsx
+++ src/fullScrenPage.tsx
@@ -1,38 +1,55 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+type Product = {
+	name: string;
+	stock: number;
+	startingStock: number;
+	price: number;
+	margin: number;
+	color: string;
+	type: string;
+};
 
 export default function FullScrenPage() {
-    return (
-        <div className="fullScren">
-            <div className='productList'>
-                {GetProductList()}
-            </div>
-            <div className='focus'>
-                <div className='focusProduct'>
-                    wating...
-                </div>
-                <div className='info'>
-                    wating...
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="fullScren">
+			<div className='productList'>
+				{ProductList()}
+			</div>
+			<div className='focus'>
+				<div className='focusProduct'>
+					wating...
+				</div>
+				<div className='info'>
+					wating...
+				</div>
+			</div>
+		</div>
+	);
+	function ProductList() {
+		const [products, setProducts] = useState<Product[]>([]);
 
-    function GetProductList() {
-        const [product, setProduct] = useState(<>wating...</>)
-        getProduct().then((value) => {
-            setProduct(value);
-        })
-        return product;
-        async function getProduct() {
-            await axios.get('https://raw.githubusercontent.com/cafeteriaVox/cafeteriaVox.github.io/dev/src/listProduct.json').then(html => {
-                // handle success
-                const page = html;
-            }).catch(error => {
-                // handle error
-                console.log(error);
-            });
-            return (<>truc</>)
-        }
-    }
+		useEffect(() => {
+			axios.get('https://raw.githubusercontent.com/cafeteriaVox/cafeteriaVox.github.io/dev/src/listProduct.json')
+				.then(response => {
+					const data = response.data;
+					console.log(data);
+					if (Array.isArray(data)) {
+						setProducts(data);
+					}
+				})
+				.catch(error => {
+					console.error(error);
+				});
+		}, []);
+
+		return (
+			<ul>
+				{products.map(product => (
+					<li key={product.name}>{product.name}</li>
+				))}
+			</ul>
+		);
+	}
 }
